501605aa03ff763a1e5842198c0f686d29743ffc
shw1226 <94415882+shw1226@users.noreply.github.com>
2021-11-16 10:17:03 +0900

    Add Data

---
+++ TraniningC/TraniningC/FormMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Krypton.Toolkit;
+
+namespace TraniningC
+{
+    public partial class FormMain : KryptonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
